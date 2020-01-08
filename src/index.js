30ba3ca64c83201627a04c8e973136e03d2e2b40
Abdul <abdul.ashraf@mountblue.tech>
2020-01-08 17:24:05 +0530

    remove bootstrap library

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import List from './components/lists/List';
