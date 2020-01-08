7f65d68b5a9e96d9647b49ead5b61f836c6368b8
Abdul <abdul.ashraf@mountblue.tech>
2020-01-08 17:46:38 +0530

    add bootstrap library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import List from './components/lists/List';
